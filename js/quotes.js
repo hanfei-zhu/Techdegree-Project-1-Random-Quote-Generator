fe2dbabe0ffcbaeb4bd500c7c26f8bb1319e7831
Hanfei Zhu <91hanfei91@gmail.com>
2017-06-22 17:32:12 +0800

    Alignment changes

--- js/quotes.js
+++ js/quotes.js
@@ -1,30 +1,36 @@
 var quotes = [
-  {quote: "It's a funny thing, the more I practice the luckier I get.",
+  {
+  quote: "It's a funny thing, the more I practice the luckier I get.",
   source: "Arnold Palmer",
   citation: "Quotlr App",
   tags: ["practice", "luck", "funny"]
   },
-  {quote: "Life begins at the end of your comfort zone.",
+  {
+  quote: "Life begins at the end of your comfort zone.",
   source: "Neale Donald Walsch",
   citation: "Quotlr App",
   tags: ["life"]
   },
-  {quote: "It's very hard to be simple enough to be good.",
+  {
+  quote: "It's very hard to be simple enough to be good.",
   source: "Ralph Waldo Emerson",
   citation: "Quotlr App",
   tags: ["goodness"]
   },
-  {quote: "Soft data, hard conflicts."
+  {
+  quote: "Soft data, hard conflicts."
   source: "Gerhard Kocher",
   citation: "Quotlr App",
   tags: ["information"]
   },
-  {quote: "Always laugh when you can. It is cheap medicine.",
+  {
+  quote: "Always laugh when you can. It is cheap medicine.",
   source: "Lord Byron",
   citation: "Quotlr App",
   tags: ["medicine", "laugh"]
   },
-  {quote: "I do not suffer from insanity, I enjoy every minute of it.",
+  {
+  quote: "I do not suffer from insanity, I enjoy every minute of it.",
   source: "Edgar Allan Poe",
   citation: "Quotlr App",
   tags: ["mind", "insanity", "suffer", "unjoy"]
