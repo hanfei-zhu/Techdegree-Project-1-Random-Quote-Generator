2b2cd121d107c68decf9a5bb6ea5b29e6a30df45
Hanfei Zhu <91hanfei91@gmail.com>
2017-06-28 23:52:52 +0800

    Started on getRandomQuote function.
    Wrote comments for function.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+function getRandomQuote(){
+  /* function selects a random quote from quotes array
+  output: returns randomly selected quotes object
+  */
+}
